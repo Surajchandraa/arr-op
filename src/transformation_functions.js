1bc7de99b172038d43980d77efd6f2cd0458e4fd
suraj <surajjosh7@gmail.com>
2023-12-01 08:00:00 +0530

    new functions added

---
+++ src/transformation_functions.js
@@ -0,0 +1,69 @@
+function filterGreaterThan(arr,num,callback){
+    let suraj=arr.filter((a)=>{
+        return a>num
+    })
+    if (suraj.length > 0) {
+        callback(null,suraj)
+    } else {
+        callback(null,"no element found");
+    }
+}
+
+function filterSmallerThan(arr,num,callback){
+    let suraj=arr.filter((a)=>{
+        return a<num;
+    })
+    if(suraj.length >0){
+        callback(null,suraj);
+    }
+    else{
+        callback(null,"no element found")
+    }
+}
+function filterEvenNumbers(arr,callback){
+    let suraj=arr.filter((a)=>{
+        if(a%2==0){
+            return a;
+        }
+        
+    })
+    if(suraj.length>0){
+        callback(null,suraj);
+    }
+    else{
+        callback(null,"no element found")
+    }
+}
+
+
+function filterOddNumbers(arr,callback){
+    let suraj=arr.filter((a)=>{
+        if(a%2!=0){
+            return a;
+        }
+        
+    })
+    if(suraj.length>0){
+        callback(null,suraj);
+    }
+    else{
+        callback(null,"no element found")
+    }
+}
+
+function removeDuplicates(arr, callback) {
+    let uniqueArray = [];
+
+    for (let i = 0; i < arr.length; i++) {
+        if (uniqueArray.indexOf(arr[i]) === -1) {
+            uniqueArray.push(arr[i]);
+        }
+    }
+
+    callback(null, uniqueArray);
+}
+
+let a = [0, -2, 1, 5, 6, 7, 8, 9, 16, 5, 1, 7, 6];
+removeDuplicates(a, (err, o) => {
+    console.log(o); 
+});
